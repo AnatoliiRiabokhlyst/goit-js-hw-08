05b43c250d832face21664772d81f80b9c149062
Anatolii <anatoliirst@gmail.com>
2022-06-02 18:07:48 +0200

    add task 2 without throtle

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import Player from '@vimeo/player';
+import Lodash from 'lodash.throttle'
 
+const iframe = document.querySelector('iframe');
+    const player = new Player(iframe);
+    
+    player.on('timeupdate', function(data) {
+    const time = data.seconds;
+    localStorage.setItem("videoplayer-current-time", time)
+});
+    const onTime = localStorage.getItem("videoplayer-current-time");
+
+    player.setCurrentTime(onTime).then(function(seconds) {
+    }).catch(function(error) {
+        switch (error.name) {
+            case 'RangeError':
+                break;
+            default:
+                break;
+        }
+    });

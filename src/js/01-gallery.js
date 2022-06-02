b1f074ac9b0363e7d8359619bb6c9432c3bfa623
Anatolii <anatoliirst@gmail.com>
2022-06-02 13:57:29 +0200

    1 task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,16 @@
-// Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";// Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below
+// Change code below this line
+const galleryRef = document.querySelector('.gallery');
 
-console.log(galleryItems);
+    const addHtml = galleryItems.map((item) =>
+    `<a class="gallery__item" href="${item.original}">
+    <img class="gallery__image" src="${item.preview}" 
+    alt="${item.description}"/>
+    </a>`
+).join("");
+
+galleryRef.insertAdjacentHTML('beforeend', addHtml)
+    let modal = new SimpleLightbox('.gallery__item', {captionsData:'alt', captionDelay:250});
+    

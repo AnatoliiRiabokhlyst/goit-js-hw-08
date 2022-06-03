5fa3d6ca96ba9a6fa5b09bc2573abd0f6d8dca9f
Anatolii <anatoliirst@gmail.com>
2022-06-03 15:57:49 +0200

    end hw 8

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,21 +1,21 @@
+// --------------------------IMPORTS---------------------------------------
 import throttle from 'lodash.throttle';
 
+// --------------------------EXPORT-----------------------------------------
 const formRef = document.querySelector('.feedback-form');
-
-formRef.addEventListener('submit', submitForm);
-formRef.addEventListener('input', throttle(validateForm, 500));
-
+const emailRef = document.querySelector('input[name="email"]');
+const messageRef = document.querySelector('textarea[name="message"]');
 const dataBase = {
     email: '',
     message: '',
 }
+
+// --------------------------FUNCTIONS-------------------------------------
 function validateForm(e){
     const { name, value } = e.target;
     dataBase[name] = value;
-
     localStorage.setItem('feedback-form-state', JSON.stringify(dataBase));
 }
-
 function submitForm (e){
     e.preventDefault();
     e.currentTarget.reset();
@@ -24,24 +24,14 @@ function submitForm (e){
 function getDataBase(){
 const getData = localStorage.getItem('feedback-form-state');
 const parsedData = JSON.parse(getData);
-console.log(parsedData);
-}
+if (parsedData) {
+    emailRef.value = parsedData.email;
+    messageRef.value = parsedData.message;
+}}
+
+// --------------------------EVENTS-----------------------------------------
+formRef.addEventListener('submit', submitForm);
+
+formRef.addEventListener('input', throttle(validateForm, 500));
+
 getDataBase();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
